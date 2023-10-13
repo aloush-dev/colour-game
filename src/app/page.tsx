21ccc732c673683e45592a087c37aab38295a172
itsaliabdallah <itsaliabdallah@gmail.com>
2023-10-13 21:12:04 +0100

    add link to portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,18 @@
 import { Header } from "./components/Header";
 import { SliderContextProvider, useSlider } from "./context/SliderContext.tsx";
 import { GameBoard } from "./components/GameBoard.tsx";
+import Link from "next/link";
 export default function Home() {
   return (
     <SliderContextProvider>
       <main className="h-screen w-screen bg-[#24293f] text-white flex flex-col">
-      <Header />
+        <Header />
         <GameBoard />
+        <div className="text-center fixed bottom-0 inset-x-0 flex justify-center items-end p-4 underline">
+          <Link href="https://www.aloush.dev" target="_blank">
+            aloush.dev
+          </Link>
+        </div>
       </main>
     </SliderContextProvider>
   );
